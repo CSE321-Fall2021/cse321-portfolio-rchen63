1c4d4e2b87a5876cf5810f2e2e538ea9e44539c1
rchen63 <70613648+rchen63@users.noreply.github.com>
2021-09-30 16:00:11 -0400

    Add files via upload

---
+++ Project 1/CSE321_project1_rchen63_template.cpp
@@ -0,0 +1,29 @@
+/**
+ * Author: Ray Chen (rchen63@buffalo.edu)
+ 
+ * Purpose: 
+ 
+ * Modules/Subroutines:
+ 
+ * The Input: 
+ 
+ * The Output:
+ 
+ * Constraints:
+ 
+ * Sourses/References used:
+ 
+**/
+
+
+
+#include "mbed.h"
+
+// main() runs in its own thread in the OS
+int main()
+{
+    while (true) {
+
+    }
+}
+
